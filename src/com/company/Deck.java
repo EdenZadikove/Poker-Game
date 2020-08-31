250ac3b4e8062262ef7e80fb7d76aa76e0ebba90
EdenZadikove <58597286+EdenZadikove@users.noreply.github.com>
2020-08-31 11:43:26 +0300

    added functions
    added dunctions to check-
    1. one pair
    2. two pairs
    3. three of a kind
    4. four of a kind
    5. full house
    6. no hand

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -3,9 +3,9 @@ package com.company;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
+
 public class Deck {
     private ArrayList<Card> cards;
-
     public Deck(){
         this.cards = new ArrayList<Card>();
     }
@@ -26,10 +26,10 @@ public class Deck {
         //Use random
         Random random = new Random();
         int randomCardIndex = 0;
-        int handSize = 5;
+        int originalSize  = this.cards.size();
         for (int i = 0; i < 5; i++) {
             //Generate random index
-            randomCardIndex = random.nextInt(5) + 1;
+            randomCardIndex = random.nextInt((this.cards.size()-1-0) + 1) + 0;
             tmpDeck.add(this.cards.get(randomCardIndex));
 
             //remove from original deck
@@ -40,11 +40,41 @@ public class Deck {
 
     public String toString() {
         String cardListOutput ="";
-        int i = 0;
+        int i = 1;
         for(Card card : this.cards) {
             cardListOutput +="\n" + i + "-" +  card.toString();
             i++;
         }
         return cardListOutput;
+    }
+
+    public int checkPair() {
+        int i, j, cardCounter = 0, pairCounter = 0 ;
+
+        for(i = 0; i < cards.size(); i++) {
+            Card tmpCard = cards.get(i);
+            cardCounter = 1;
+            for(j = i+1; j < cards.size(); j++) {
+                if(tmpCard == cards.get(j)) cardCounter++; //congratulation you got a pair
+            }
+            if(cardCounter == 2)
+                pairCounter++;
+        }
+        return pairCounter;
+    }
+
+    public int checkTreeOrFour() {
+        int i, j, cardCounter = 0;
+
+        for(i = 0; i < cards.size(); i++) {
+            Card tmpCard = cards.get(i);
+            cardCounter = 1;
+            for(j = i+1; j < cards.size(); j++) {
+                if(tmpCard == cards.get(j)) cardCounter++; //congratulation you got a pair
+            }
+            if(cardCounter == 3 || cardCounter == 4)
+               return cardCounter;
+        }
+        return 0; //no three of a kind or four of a kind
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,33 @@ public class Main {
         playingDeck.getCards();
         playingDeck.shuffle();
 
-        System.out.println(playingDeck);
+
+        //check for 1 pair of two pairs
+        int pairCounter = playingDeck.checkPair();
+        //check for three of a kind
+        int threeOrFour = playingDeck.checkTreeOrFour();
+
+
+        //print results
+        System.out.println(playingDeck +"\n");
+
+        if(pairCounter != 0)
+            System.out.println("You got " + pairCounter + " pairs.");
+
+        switch (threeOrFour){
+            case 3:
+                System.out.println("You got three of a kind");
+                break;
+            case 4:
+                System.out.println("You got four of a kind");
+                break;
+            default:
+                if(pairCounter == 0 && threeOrFour == 0)
+                   System.out.println("Sorry, you got no hand");
+                else if(pairCounter == 1 && threeOrFour == 3) //one pair and one three of a kind
+                    System.out.println("Congratulation, you got a Full House");
+        }
+
 
     }
 }
